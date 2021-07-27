f079079f7fec946c478be402f532861f037fe064
saj <aejeong-context@kakao.com>
2021-07-27 16:56:17 +0900

    [mod] add scroll view
    diary 에 상세가 추가되면서 생겨나는 칸 때문에
    스크롤 뷰 추가.

--- app/src/main/java/com/example/alroha/diary/AddDiary.java
+++ app/src/main/java/com/example/alroha/diary/AddDiary.java
@@ -2,6 +2,7 @@ package com.example.alroha.diary;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,36 +35,50 @@ public class AddDiary extends Fragment implements onBackPressListener {
         viewGroup = (ViewGroup) inflater.inflate(R.layout.diary_add, container, false);
         addPlace = viewGroup.findViewById(R.id.addPlace);
         addPlace.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 System.out.println("다이~");
+
+                // 추가될 장소인 레이아웃
                 LinearLayout dateView = (LinearLayout) viewGroup.findViewById(R.id.diary_card);
 
+
                 CardView cardView = new CardView(getActivity().getApplicationContext());
+                CardView cardView2 = new CardView(getActivity().getApplicationContext());
 
-                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-
-
-
-                params.topMargin = 35;
-                params.leftMargin = 20;
 
                 EditText placeEdit = new EditText(getActivity().getApplicationContext());
                 placeEdit.setHint("번째 장소이름");
+                placeEdit.setTextSize(15);
+                placeEdit.setBackground(null);
                 placeEdit.setHintTextColor(Color.parseColor("#DDDDDD"));
-
-                EditText commentEdit = new EditText(getActivity().getApplicationContext());
-                commentEdit.setHint("한줄평");
-                commentEdit.setHintTextColor(Color.parseColor("#DDDDDD"));
-
-
-                cardView.addView(commentEdit);
                 cardView.addView(placeEdit);
 
+
+                // 카드
+                LinearLayout.LayoutParams cardParam = new LinearLayout.LayoutParams(1000, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+                cardView.setLayoutParams(cardParam);
+
+                cardView.setCardBackgroundColor(Color.WHITE);
+                cardView.setCardElevation(0);
+
+
+
+                EditText commentEdit = new EditText(getActivity().getApplicationContext());
+
+                commentEdit.setHint("한줄평");
+                commentEdit.setTextSize(15);
+                placeEdit.setBackground(null);
+                commentEdit.setHintTextColor(Color.parseColor("#DDDDDD"));
+
+                cardView2.addView(commentEdit);
+
+                // 레이아웃에 카드 넣기
+
                 dateView.addView(cardView);
-
-
-
+                dateView.addView(cardView2);
 
 
             }
